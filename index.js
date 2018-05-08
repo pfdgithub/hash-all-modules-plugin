8f0b120e7e6de2923dea05d13374743fce864d6f
liping <liping@wdai.com>
2018-05-08 18:07:27 +0800

    支持 webpack 4

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
-const createHash = require("crypto").createHash;
+var crypto = require("crypto");
+var PLUGIN_NAME = 'HashAllModulesPlugin';
 
-class HashAllModulesPlugin {
-  constructor(options) {
-    this.options = Object.assign({
-      hashFunction: "md5",
-      hashDigest: "base64",
-      hashDigestLength: 4
-    }, options);
-  }
+function HashAllModulesPlugin(options) {
+  this.options = Object.assign({
+    hashFunction: "md5",
+    hashDigest: "base64",
+    hashDigestLength: 4
+  }, options);
+}
 
-  apply(compiler) {
-    const options = this.options;
-    compiler.plugin("compilation", (compilation) => {
+HashAllModulesPlugin.prototype.apply = function (compiler) {
+  var self = this;
+
+  // webpack 4
+  if (compiler.hooks) {
+    compiler.hooks.compilation.tap(PLUGIN_NAME, function (compilation) {
       const usedIds = new Set();
-      compilation.plugin("before-module-ids", (modules) => {
-        modules.forEach((module) => {
-          if (module.id !== null) {
-            return;
-          }
-
-          const id = module.identifier();
-          const hash = createHash(options.hashFunction);
-          hash.update(id);
-          const hashId = hash.digest(options.hashDigest);
-          let len = options.hashDigestLength;
-          while (usedIds.has(hashId.substr(0, len)))
-            len++;
-          module.id = hashId.substr(0, len);
-          usedIds.add(module.id);
-        });
+      compilation.hooks.beforeModuleIds.tap(PLUGIN_NAME, function (modules) {
+        self.hash(usedIds, modules);
       });
     });
   }
+  else {
+    compiler.plugin("compilation", (compilation) => {
+      const usedIds = new Set();
+      compilation.plugin("before-module-ids", function (modules) {
+        self.hash(usedIds, modules);
+      });
+    });
+  }
+}
+
+HashAllModulesPlugin.prototype.hash = function (usedIds, modules) {
+  var options = this.options;
+
+  modules.forEach(function (module) {
+    if (module.id !== null) {
+      return;
+    }
+
+    const id = module.identifier();
+    const hash = crypto.createHash(options.hashFunction);
+    hash.update(id);
+    const hashId = hash.digest(options.hashDigest);
+    let len = options.hashDigestLength;
+    while (usedIds.has(hashId.substr(0, len)))
+      len++;
+    module.id = hashId.substr(0, len);
+    usedIds.add(module.id);
+  });
 }
 
 module.exports = HashAllModulesPlugin;
